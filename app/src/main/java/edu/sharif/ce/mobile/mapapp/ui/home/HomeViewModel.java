284b927fbbeb7758a46858834bd66f6d3a4ae745
Seyyed Parsa Neshaei <spneshaei@gmail.com>
2021-04-15 07:55:23 +0430

    First Commit

---
+++ app/src/main/java/edu/sharif/ce/mobile/mapapp/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package edu.sharif.ce.mobile.mapapp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
