689f5c2743b7aec11dc54c3fc3fcef61dc96e203
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-04-15 08:26:43 +0430

    Changes to MVC

--- app/src/main/java/edu/sharif/ce/mobile/mapapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/sharif/ce/mobile/mapapp/ui/notifications/NotificationsFragment.java
@@ -16,20 +16,11 @@ import edu.sharif.ce.mobile.mapapp.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        textView.setText("hi");
         return root;
     }
 }
